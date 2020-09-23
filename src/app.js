0952d434ff9bfa6584a20c2c9be98be350f11b11
unknown <lucas.ambrosio.am@gmail.com>
2020-09-22 22:29:09 -0300

    add music

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ let scoreDisplay;
 let squares;
 let gameOverImg;
 let oImg, ooImg, oooImg, ooooImg;
-let playButton;
+let playButton, musicButton;
+let buttonImg;
+
+let music = new Audio("./assets/musiquinhamanera.mp3");
+music.loop = true;
 
 document.addEventListener("DOMContentLoaded", () => {
   scoreDisplay = document.getElementById("score");
@@ -13,6 +17,23 @@ document.addEventListener("DOMContentLoaded", () => {
   oooImg = document.getElementById("ooo");
   ooooImg = document.getElementById("oooo");
   playButton = document.getElementById("start-button");
+  musicButton = document.getElementById("music-button");
+  buttonImg = document.getElementById("button-img");
+
+  musicButton.addEventListener("click", () => {
+    const state = musicButton.getAttribute("data-state");
+    if (state === "mute") {
+      musicButton.setAttribute("data-state", "nonmute");
+      buttonImg.setAttribute("src", "./assets/volume-up.svg");
+
+      music.play();
+    } else {
+      musicButton.setAttribute("data-state", "mute");
+      buttonImg.setAttribute("src", "./assets/no-sound.svg");
+
+      music.pause();
+    }
+  });
 });
 
 const draw = () => {
